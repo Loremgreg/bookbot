9b5f9c6f9aa63ee8cee508176e7544baad2636ea
Loremgreg <gregorybastin.de@gmail.com>
2025-06-17 11:51:29 +0200

    print a report in a structure

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, sort_characters_by_count
 
 def get_book_text(book_path):                           # Fonction qui prend un chemin de fichier en paramètre
     with open(book_path, 'r', encoding="utf-8") as f:   # la methode pour ouvrir et lire un fichier -> Reading and Writing Files : f = open('workfile', 'r', encoding="utf-8") r= reading, w= writing, etc --> Ouvre le fichier en mode lecture avec l'encodage UTF-8
@@ -10,7 +10,14 @@ def main():                                             # Fonction principale qu
     contents = get_book_text("books/frankenstein.txt")  # Lit le contenu du livre Frankenstein et le stocke dans 'contents'
     count_result = count_characters(contents)           # Compte chaque caractère (lettres, espaces, symboles, etc.) dans le contenu
     num_words = count_words(contents)                   # Compte les mots dans le contenu du livre
-    print(f'{num_words} words found in the document')   # Affiche le nombre de mots trouvés dans le document
-    print(count_result)                                 # Affiche le dictionnaire des fréquences des caractères
+    sorted = sort_characters_by_count(count_result)
+    print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...")
+    print(f'----------- Word Count ----------\nFound {num_words} total words')   # Affiche le nombre de mots trouvés dans le document
+    print("--------- Character Count -------\n")
+    for result in sorted:
+        if result["char"].isalpha() == True:
+            print(f"{result['char']}: {result['num']}")
+    print("============= END ===============")
+                                                        # Affiche le dictionnaire des fréquences des caractères
 main()                                                  # Appelle la fonction principale pour exécuter le programme
 
