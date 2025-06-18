d5c21e7049bdc9b748cb52afa8b11ea1992df180
Loremgreg <gregorybastin.de@gmail.com>
2025-06-18 07:43:41 +0200

    Arguments: using sys.argv

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words, count_characters, sort_characters_by_count
 
 def get_book_text(book_path):                           # Fonction qui prend un chemin de fichier en paramètre
@@ -7,11 +8,20 @@ def get_book_text(book_path):                           # Fonction qui prend un 
 
 
 def main():                                             # Fonction principale qui appelle get_book_text avec un chemin relatif vers le document a lire
-    contents = get_book_text("books/frankenstein.txt")  # Lit le contenu du livre Frankenstein et le stocke dans 'contents'
+   #contents = get_book_text("books/frankenstein.txt")  # Lit le contenu du livre Frankenstein et le stocke dans 'contents'
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        contents = get_book_text(sys.argv[1])
+
     count_result = count_characters(contents)           # Compte chaque caractère (lettres, espaces, symboles, etc.) dans le contenu
     num_words = count_words(contents)                   # Compte les mots dans le contenu du livre
     sorted = sort_characters_by_count(count_result)
-    print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...")
+
+    
+
+    print(f"============ BOOKBOT ============\nAnalyzing book found at {sys.argv[1]}")
     print(f'----------- Word Count ----------\nFound {num_words} total words')   # Affiche le nombre de mots trouvés dans le document
     print("--------- Character Count -------\n")
     for result in sorted:
